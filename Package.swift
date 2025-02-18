2232d63cb399bd13dbaa55a0c462064c54f5e1e5
Mati <matiasvillaverde@protonmail.com>
2025-02-18 21:50:25 +0100

    Update Package.swift (#1)
    * Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             targets: ["SentencepieceTokenizer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
     ],
     targets: [
         .executableTarget(
